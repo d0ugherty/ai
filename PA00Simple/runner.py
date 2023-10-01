abf50a4f9dde146d72ee2d33ff6222c9e83d3305
d0ugherty <tdougherty16@gmail.com>
2023-10-01 15:07:07 -0400

    previous assignments

---
+++ PA00Simple/runner.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+ 
+from tiny import printUserID
+
+print("Let's go ...")
+printUserID()
---
+++ PA00Simple/tiny.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def printUserID():
+    print()
+    print("My UserID is doughe38")
+    print()
+
+if __name__ == '__main__':
+    printUserID()
+
